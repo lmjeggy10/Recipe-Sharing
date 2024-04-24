174fc09e94317f4e290043df943b872043bb44d7
liyajeggy <liyajeggy@gmail.com>
2024-04-24 20:35:54 +0530

    Final commit

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -17,12 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 # from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='recipes/', permanent=False)),
+    path('recipes/', include('recipes.urls')),
     # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('', include('recipes.urls')),
 ]
 
 if settings.DEBUG:
--- recipes/urls.py
+++ recipes/urls.py
@@ -4,10 +4,10 @@ from . import views
 from .views import send_recipe_email
 
 urlpatterns = [
-    path('recipes/', views.RecipeListCreate.as_view(), name='recipe-list-create'),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe-detail'),
-    path('recipes/new/', views.RecipeCreate.as_view(), name='recipe-create'), 
-    path('recipes/<int:pk>/edit/', views.RecipeUpdate.as_view(), name='recipe-update'),
+    path('', views.RecipeListCreate.as_view(), name='recipe-list-create'),
+    path('<int:pk>/', views.RecipeDetail.as_view(), name='recipe-detail'),
+    path('new/', views.RecipeCreate.as_view(), name='recipe-create'), 
+    path('<int:pk>/edit/', views.RecipeUpdate.as_view(), name='recipe-update'),
     # path('send-email/', send_email_view, name='send_recipe_email'),
     path('send-recipe-email/', send_recipe_email, name='send_recipe_email'),
     # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token authentication
